ab35e50521fa6bb15ec86bad1a009ad254c99c87
planetwuztaken <planetisnotgay@proton.me>
2026-06-05 13:05:02 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,6 @@
-
+const hetorgay = document.getelementbyid("hetorgay");
+const gaybutton = document.getelementbyid("gaybutron");
+function becomegay(){
+  hetorgay.innertext = "nah bro im gay";
+}
+gaybutton.addeventlistener("click", becomegay);
